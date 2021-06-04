e3b6ec627f29155e02aed86e4c58e6fece7acefd
florisweb <floris@florisweb.tk>
2021-06-04 14:35:47 +0200

    Optimized the meshes

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
 				0.1,
 				1000
 			);
-			camera.position.z = 70;
+			camera.position.z = 70; //70;
 			camera.position.x = -20;
 			camera.position.y = 20;
 			// camera.rotation.set(0, -.7, 0);
@@ -58,28 +58,47 @@
 
 
 
+			let worldSize = 100;
+			let tileCount = 200;
 
-
-			let tiles = [];
 			let material1 = new THREE.MeshLambertMaterial({color: 0x00FF77});
 			let material2 = new THREE.MeshLambertMaterial({color: 0x999999});
-			for (let x = 0; x < 50; x++)
+			
+			let geometry1 = new THREE.Geometry();
+			let geometry2 = new THREE.Geometry();
+			for (let x = 0; x < tileCount; x++)
 			{
-				tiles[x] = [];
-				for (let z = 0; z < 50; z++)
+				for (let z = 0; z < tileCount; z++)
 				{
 					let height = Math.random() * 1 + 1;
-					let geometry = new THREE.BoxGeometry(1.0, height, 1.0);
-					let material = Math.random() > .5 ? material1 : material2;
-					let mesh = new THREE.Mesh(geometry, material);
-					mesh.position.x = x;
-					mesh.position.z = z;
-					mesh.position.y = height / 2;
-					scene.add(mesh);
-					tiles[x][z] = mesh;
+					let geometry = new THREE.BoxGeometry(worldSize / tileCount, height, worldSize / tileCount);
+					let Mesh = new THREE.Mesh(geometry);
+					Mesh.position.x = x * worldSize / tileCount;
+					Mesh.position.z = z * worldSize / tileCount;
+					Mesh.position.y = height / 2;
+					let type2 = Math.random() > .5;
+					if (type2)
+					{
+						geometry2.mergeMesh(Mesh);
+						continue;
+					} 
+					geometry1.mergeMesh(Mesh);
 				}
 			}
 
+			geometry1.mergeVertices();
+			geometry2.mergeVertices();
+			
+			let mesh1 = new THREE.Mesh(geometry1, material1);
+			mesh1.position.x = -worldSize / 2;
+			mesh1.position.z = -worldSize / 2;
+
+			let mesh2 = new THREE.Mesh(geometry2, material2);
+			mesh2.position.x = -worldSize / 2;
+			mesh2.position.z = -worldSize / 2;
+
+			scene.add(mesh1);
+			scene.add(mesh2);
 
 
 			let light = new THREE.PointLight(0xffffff, 1, 500);
@@ -92,7 +111,7 @@
 			function render() {
 				renderer.render(scene, camera);
 				camera.position.x += .1;
-				camera.lookAt(25, 0, 25);
+				camera.lookAt(0, 0, 0);
 
 				requestAnimationFrame(render);
 			}
